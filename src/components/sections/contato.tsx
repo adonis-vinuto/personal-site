d9bc0ccd3bc7fe3a683062bb125f049348bcb0bf
Adonis Vinuto <vinuto@adonis.dev.br>
2025-08-09 23:40:07 -0300

    Feat (HomePage): faltou mais esses arquivos antes de focar em correções de detalhes

--- src/components/sections/contato.tsx
+++ src/components/sections/contato.tsx
@@ -2,8 +2,8 @@
 
 export default function Contato() {
   return (
-    <div className="text-center px-8 max-w-4xl">
-      <h1 className="text-5xl md:text-6xl font-bold mb-4 text-gray-900">
+    <div className="container-responsive">
+      <h1 className="title-h1">
         Contato
       </h1>
     </div>
